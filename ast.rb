d5c549d739dae390ca29c28c7a400641a067651e
Peter Arato <it.arato@gmail.com>
2023-08-08 20:51:45 -0400

    Try to build ast but not quite sure how

---
+++ ast.rb
@@ -0,0 +1,26 @@
+require_relative("util")
+
+class AstNode
+  attr_reader(:children)
+  attr_accessor(:parent)
+
+  def initialize(rule_or_token)
+    @rule_or_token = rule_or_token
+    @children = []
+    @parent = nil
+  end
+
+  def add_child(rule_or_token)
+    self.class.new(rule_or_token).tap do |node|
+      node.parent = self
+      @children.push(node)
+    end
+  end
+
+  def reject
+    panic!("Rejecting root node") if @parent.nil?
+
+    @parent.children.delete(self)
+    @parent
+  end
+end
